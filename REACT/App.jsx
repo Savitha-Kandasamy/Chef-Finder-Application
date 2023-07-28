b4ede804e3f603c45f32e199dff532005dc4f31d
Savitha-Kandasamy <113970076+Savitha-Kandasamy@users.noreply.github.com>
2023-07-28 10:29:35 +0530

    Add files via upload

---
+++ REACT/App.jsx
@@ -0,0 +1,32 @@
+import './App.css';
+import {BrowserRouter as Router, Route,Routes}from 'react-router-dom';
+import Login from './pages/login';
+import RegistrationForm from './pages/rform';
+import Sidebar from './components/sidebar';
+import Home from './pages/home';
+import About from './pages/about';
+import Dashboard from './pages/dashboard';
+function App() {
+  return (
+    <div className="App">
+    <Router>
+
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/Signup" element={<RegistrationForm/>}/>
+        <Route path="/Sidebar" element={<Sidebar/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/login" element={<Login/>} />
+
+        
+
+      
+      </Routes>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
